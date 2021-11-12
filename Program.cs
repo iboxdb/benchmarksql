9a4e2bb5ba03d5d8e51336c42de1bc1bd8d76f5e
Bruce <iboxdb@users.noreply.github.com>
2021-11-12 03:21:50 -0500

    clear

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace BenchmarkSQL
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Benchmark Version 1.1.5");
+            Console.WriteLine("Benchmark Version 1.1.6");
 
             //root = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
             root = "../";
@@ -484,7 +484,7 @@ namespace BenchmarkSQL
         public class T1
         {
             public int Id { get; set; }
-            public string? Value;
+            public string Value;
         }
 
         class AppServer : LocalDatabaseServer
